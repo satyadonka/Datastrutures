6e9aa203503709832864e9e890881d0c446348e6
Samuel <jawahar.samuel@gmail.com>
2017-03-20 13:44:51 +0530

    Non repeating charectors sub string
    works in order of n

---
+++ NRCS.java
@@ -0,0 +1,33 @@
+package dataStructures;
+
+public class NRCS {
+	public String getNRCStr(String data) {
+		String result = "", aux = "";
+		int table[] = new int[257];
+		int len = data.length();
+		int ascii = 0, prevLimit = len + 2;
+		int j = 0;
+		for (j = len - 1; j >= 0; j--) {
+			ascii = (int) data.charAt(j);
+			if (table[ascii] != 0 && table[ascii] <= prevLimit) {
+				aux = data.substring(j + 1);
+				if (aux.length() > result.length()) {
+					result = aux;
+				}
+				data = data.substring(0, table[ascii]);
+				len = data.length();
+				prevLimit = table[ascii];
+			}  
+			table[ascii] = j;
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NRCS nrcs = new NRCS();
+		System.out.println(nrcs.getNRCStr("asdfghaqwertyuiopadfghjklalkjh"));
+	}
+
+}
