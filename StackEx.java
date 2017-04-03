5092e4bc29f1640ef3e4c0203c0530d193294dd9
Samuel <jawahar.samuel@gmail.com>
2017-04-03 16:58:18 +0530

    this is to implement recurrion using stack
    all trvesals and kth max element in BST are implemented using stack

---
+++ StackEx.java
@@ -0,0 +1,162 @@
+
+import java.util.Stack;
+
+public class StackEx {
+	public String findkthMaxElement(TreeUtil.Node root, int k) {
+		int loc=0;
+		Stack callSt = new Stack();
+		callSt.push(root);
+		TreeUtil.Node aux = null;
+
+		while (!callSt.isEmpty()) {
+			aux = (TreeUtil.Node) callSt.pop();
+			if (aux.left == null && aux.right == null) {
+				if(!"NULL".equals(aux.data)){
+					loc++;
+					if(loc==k){
+						return aux.data;
+					}
+				}
+				if (!callSt.isEmpty()) {
+					aux = (TreeUtil.Node) callSt.pop();
+					if(!"NULL".equals(aux.data)){
+						loc++;
+						if(loc==k){
+							return aux.data;
+						}
+					}
+				}
+				continue;
+			}
+			if (aux.right != null) {
+				callSt.push(aux.right);
+			}
+			if (aux != null) {
+				callSt.push(aux);
+			}
+			if (aux.left != null) {
+				callSt.push(aux.left);
+			}
+
+		}
+
+	return "";
+	}
+
+	public static void postorderSuccOf(TreeUtil.Node root, String ele) {
+		// Navigate to left,Navigate to Right,Process
+		Stack callSt = new Stack();
+		callSt.push(root);
+
+		while (!callSt.isEmpty()) {
+
+		}
+
+	}
+
+	public void printinPostorder(TreeUtil.Node root) {
+		Stack callSt = new Stack();
+		callSt.push(root);
+		TreeUtil.Node aux = null;
+		while (!callSt.isEmpty()) {
+			aux = (TreeUtil.Node) callSt.pop();
+			if (aux.right == null && aux.left == null) {
+				if(!"NULL".equals(aux.data)){
+					System.out.print(aux.data);
+				}
+				while (!callSt.isEmpty() && (((TreeUtil.Node) callSt.peek()).right != null)
+						&& ((TreeUtil.Node) callSt.peek()).right.data.equals(aux.data)) {
+					aux = (TreeUtil.Node) callSt.pop();
+					if(!"NULL".equals(aux.data)){
+						System.out.print(aux.data);
+					}
+				}
+				continue;
+			}
+			if (aux != null) {
+				callSt.push(aux);
+			}
+			if (aux.right != null) {
+				callSt.push(aux.right);
+			}
+			if (aux.left != null) {
+				callSt.push(aux.left);
+			}
+
+		}
+	}
+
+	public void printinPreOrder(TreeUtil.Node root) {
+		Stack callSt = new Stack();
+		callSt.push(root);
+		TreeUtil.Node aux = null;
+
+		while (!callSt.isEmpty()) {
+			aux = (TreeUtil.Node) callSt.pop();
+			if(!"NULL".equals(aux.data)){
+				System.out.print(aux.data);
+			}
+			if (aux.left == null && aux.right == null) {
+				continue;
+			}
+			if (aux.right != null) {
+				callSt.push(aux.right);
+			}
+			if (aux.left != null) {
+				callSt.push(aux.left);
+			}
+
+		}
+
+	}
+
+	public void printinPInrder(TreeUtil.Node root) {
+		Stack callSt = new Stack();
+		callSt.push(root);
+		TreeUtil.Node aux = null;
+
+		while (!callSt.isEmpty()) {
+			aux = (TreeUtil.Node) callSt.pop();
+			if (aux.left == null && aux.right == null) {
+				if(!"NULL".equals(aux.data)){
+					System.out.print(aux.data);
+				}
+				if (!callSt.isEmpty()) {
+					aux = (TreeUtil.Node) callSt.pop();
+					if(!"NULL".equals(aux.data)){
+						System.out.print(aux.data);
+					}
+				}
+				continue;
+			}
+			if (aux.right != null) {
+				callSt.push(aux.right);
+			}
+			if (aux != null) {
+				callSt.push(aux);
+			}
+			if (aux.left != null) {
+				callSt.push(aux.left);
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeUtil tu = new TreeUtil();
+		StackEx se = new StackEx();
+		System.out.print("The post order:");
+		se.printinPostorder(tu.root);
+
+		System.out.print("\nThe pre order:");
+		se.printinPreOrder(tu.root);
+
+		System.out.print("\nThe In order:");
+		se.printinPInrder(tu.root);
+
+		System.out.println("\nThe kth Max values ..." + se.findkthMaxElement(tu.root, 3));
+	}
+
+}
