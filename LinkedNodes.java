5b8430b6cdf817405e50b91a706fb20f9ab267b1
Samuel <jawahar.samuel@gmail.com>
2017-04-06 15:21:30 +0530

    Update LinkedNodes.java

--- LinkedNodes.java
+++ LinkedNodes.java
@@ -5,9 +5,9 @@ public class LinkedNodes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedNodes ln = new LinkedNodes();
-		Node ptr = ln.getLinkedListof(10);
+		Node ptr = ln.getLinkedListof(2);
 		ln.printList(ptr);
-		Node sptr=ln.swapElements(ptr);
+		Node sptr = ln.swapElements(ptr);
 		ln.printList(sptr);
 	}
 
@@ -22,27 +22,44 @@ public class LinkedNodes {
 
 	public Node swapElements(Node ptr) {
 		/**
-		 * Call by Values reference...
+		 * Call by Values reference... Thumb rule as long as we are not using
+		 * dot operator on left side of the assignment we are not modifying the
+		 * list
 		 */
 		Node start = new Node();
-		Node ptr2 = new Node();
-		start.next = ptr;
 
+		Node ptr2 = new Node();
+		/**
+		 * start is an extra pointer that points the start element of the list
+		 * One Node preceded to start
+		 */
+		start.next = ptr;
+		if (ptr.next == null) {
+			return ptr;
+		}
 		Node aux = start.next.next;
+		/**
+		 *  It iis just pointing from 2nd element of the list
+		 */
 		aux = aux.next;
+		/**
+		 *  it is just pointing the 3rd element of the list
+		 */
 		Node n1 = start.next;
 		Node n2 = n1.next;
 		start.next = n2;
 		start.next.next = n1;
 		start.next.next.next = aux;
-
+		/**
+		 * ptr2 is an alias to the start pointer ,even though start pointer
+		 * changes it keeps the start of the list
+		 */
 		ptr2 = start;
 		start = start.next.next;
 
-		
-		while(start.next!=null){
+		while (start.next != null) {
 			aux = start.next.next;
-			if(aux==null){
+			if (aux == null) {
 				break;
 			}
 			aux = aux.next;
@@ -51,9 +68,9 @@ public class LinkedNodes {
 			start.next = n2;
 			start.next.next = n1;
 			start.next.next.next = aux;
-			
+
 			start = start.next.next;
-				
+
 		}
 		return ptr2.next;
 	}
